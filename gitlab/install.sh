c758384b90bfc5720505d9a65bab6e0e6b764695
root <root@nemonik.com>
2021-10-26 12:33:48 -0400

    fixes to address gitlab race conditions

--- gitlab/install.sh
+++ gitlab/install.sh
@@ -63,15 +63,42 @@ loop=0
 while : ; do
 
   if [ $loop -eq 15 ]; then
-    error "Failed post step."
-    break
+    warn
+    warn "GitLab appears to have failed to come up in the expected amount on time. Abnormal, but this doesn't mean it will never come up. The automation will keep trying til either GitLab comes up or you interrupt the automation."
+    warn
   fi
 
   if curl --silent ${gitlab_protocol}://${gitlab_fdqn} | grep -q "sign_in"; then
+    notify "Performing post ready configuration setup..."
+
+    notify "Creating GitLab automation token..."
 
     read gitlab_pod_name gitlab_token < <(create_automation_token)
 
-    curl --silent --request PUT --header "PRIVATE-TOKEN: $gitlab_token" "${gitlab_protocol}://${gitlab_fdqn}/api/v4/application/settings?${gitlab_settings}" | jq '.'
+    notify "Configuring GitLab via REST api..."
+
+    rest_api_loop=0
+    for (( ; ; )); do
+
+      if [ $rest_api_loop -eq 15 ]; then
+        warn
+        warn "GitLab REST API is refusing to let us to connect.  The autonation will keep trying til either this works or you interrupt the automation."
+        warn
+      fi
+
+      output=`curl --silent --request PUT --header "PRIVATE-TOKEN: $gitlab_token" "${gitlab_protocol}://${gitlab_fdqn}/api/v4/application/settings?${gitlab_settings}" | jq '.' 2>&1`
+      if [[ "${output}" == *"401 Unauthorized"* ]] || [[ "${output}" == *"Invalid numeric literal"* ]]; then
+        echo $output | jq -r "."
+      else
+        echo $output | jq -r "."
+        break
+      fi
+
+      sleep 5
+
+      notify "   Attempting again..."
+      ((rest_api_loop++))
+    done
 
     revoke_automation_token $gitlab_pod_name $gitlab_token
 
@@ -79,7 +106,9 @@ while : ; do
     break
   fi
 
-  notify "Still waiting for GitLab to respond to ${gitlab_protocol} requests..."
-  ((loop ++))
+  notify "  Still waiting for GitLab to respond to ${gitlab_protocol} requests..."
+  ((loop++))
   sleep 60
 done
+
+notify "Done."
